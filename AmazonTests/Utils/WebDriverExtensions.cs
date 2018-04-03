77a20a27d2db52de9a865bac6d61f54334224ecf
Andrei Arekhva <andrei_arekhva@epam.com>
2018-04-03 10:26:43 +0300

    Update packages for AmazonTests project

--- AmazonTests/Utils/WebDriverExtensions.cs
+++ AmazonTests/Utils/WebDriverExtensions.cs
@@ -5,33 +5,33 @@ using OpenQA.Selenium.Support.Extensions;
 
 namespace AmazonTests.Utils
 {
-    public static class WebDriverExtensions
-    {
-        private static WebDriverWait Wait(this IWebDriver driver, int seconds)
-        {
-            return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
-        }
+	public static class WebDriverExtensions
+	{
+		private static WebDriverWait Wait(this IWebDriver driver, int seconds)
+		{
+			return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+		}
 
-        public static void ImplicitlyWait(this IWebDriver driver, int seconds)
-        {
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(seconds));
-        }
+		public static void ImplicitlyWait(this IWebDriver driver, int seconds)
+		{
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(seconds);
+		}
 
-        public static void WaitUntilPageLoad(this IWebDriver driver, int seconds = 10)
-        {
-            driver.Wait(seconds).Until(drv => drv.ExecuteJavaScript<string>("return document.readyState").ToLower() == "complete");
-        }
+		public static void WaitUntilPageLoad(this IWebDriver driver, int seconds = 10)
+		{
+			driver.Wait(seconds).Until(drv => drv.ExecuteJavaScript<string>("return document.readyState").ToLower() == "complete");
+		}
 
-        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
-        {
-            try
-            {
-                return driver.Wait(timeoutInSeconds).Until(ExpectedConditions.ElementExists(by));
-            }
-            catch
-            {
-                return null;
-            }
-        }
-    }
+		public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+		{
+			try
+			{
+				return driver.Wait(timeoutInSeconds).Until(ExpectedConditions.ElementExists(by));
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
 }
